fc379090e3505e8b96e0322b4e98c7d3fce1bee2
Allen Shibu <allenshibuuk@gmail.com>
2024-09-18 07:01:44 +0100

    added nesting to radar parameter dictionary

--- run_ExRaM.py
+++ run_ExRaM.py
@@ -15,18 +15,53 @@ from coordinateTransforms.functions import LLA2ECEF, ECEF2LLA
 
 # Radar Parameters - Positioning & Orientation
 radar = {}
-radar['geod_pos'] = [0,0,0]
-radar['ECEF_pos'] = LLA2ECEF(radar['geod_pos'],'SPHERICAL')
-radar['boresight'] = [45,90]
+
+radar['pos_ori'] = {}
+radar['pos_ori']['geod_pos'] = [0,0,0]
+radar['pos_ori']['ECEF_pos'] = LLA2ECEF(radar['posit_and_orient']['geod_pos'],'SPHERICAL')
+radar['pos_ori']['boresight'] = [45,90]
+
+# Radar Parameters - Transmit
+radar['transmit'] = {}
+radar['transmit']['peak_power'] = 1e3
+radar['transmit']['peak_gain'] = 30
+radar['transmit']['tx_loss'] = 7
 
 # Radar Parameters - Waveform
-radar['waveform'] = 'LFM'
-radar['sample_rate'] = 100e6
-radar['carrier_frequency'] = 9.4e9
-radar['bandwidth'] = 20e6
-radar['PRF'] = 10e3
-radar['pulse_width'] = 1e-6
-radar['tx_power'] = 1e3
-radar['tx_antenna_gain'] = 30
+radar['waveform'] = {}
+radar['waveform']['sample_rate'] = 100e6
+radar['waveform']['carrier_frequency'] = 9.4e9
+radar['waveform']['bandwidth'] = 20e6
+radar['waveform']['PRF'] = 10e3
+radar['waveform']['pulse_width'] = 1e-6
+radar['waveform']['modulation_type'] = 'LFM'
+
+# Radar Parameters - Signal Processing
+radar['sig_proc'] = {}
+radar['sig_proc']['pulse_compression'] = {}
+radar['sig_proc']['pulse_compression']['toggle'] = True
+radar['sig_proc']['pulse_compression']['type'] = 'MF'
+
+radar['sig_proc']['CFAR'] = {}
+radar['sig_proc']['CFAR']['toggle'] = True
+radar['sig_proc']['CFAR']['type'] = 'CA'
+
+radar['sig_proc']['pulse_integration'] = {}
+radar['sig_proc']['pulse_integration']['toggle'] = True
+radar['sig_proc']['pulse_integration']['type'] = 'COHERENT'
+
+# Scene Parameters
+scene = {}
+scene['noise_figure'] = 5
+scene['atmos_atten'] = True
+
+# Target Parameters
+target = {}
+target['range'] = 10e3
+target['velocity'] = 100
+target['RCS'] = 1
+target['angle_offset_azi'] = 0
+target['angle_offset_elev'] = 0
+
 
 print(radar)
